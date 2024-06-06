47507ffe329558f7846210fb87a3235266702d93
Zac Watts <zwatts@deseretdigital.com>
2024-06-06 14:22:38 -0500

    team 3

--- app/components/Screen.tsx
+++ app/components/Screen.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 export default function Screen(props: PropsWithChildren) {
+  //team 3 baby
   return (
     <div className="bg-black min-h-96 w-full p-8 text-white rounded-3xl flex flex-col gap-4">
       {props.children}
